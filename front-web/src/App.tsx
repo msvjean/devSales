fb1bd8f4ba468026bcb07813cce6cabd76229229
Jean Marcel <jean.msv@gmail.com>
2022-06-28 16:38:24 -0300

    Add PieChartCad component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Filter from './components/filter';
 import Header from './components/header';
+import PieChartCard from './components/pie-chart-card';
 import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
 
@@ -13,6 +14,16 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard
+            name="Lojas"
+            labels={['Uberlandia', 'Rio de Janeiro', 'Sao Paulo', 'Porto Alegre']}
+            series={[40, 30, 10, 20]}
+          />
+          <PieChartCard
+            name="Pagamentos"
+            labels={['Crédito', 'Débito', 'Dinheiro']}
+            series={[50, 40, 10]}
+          />
         </div>
       </div>
     </>
